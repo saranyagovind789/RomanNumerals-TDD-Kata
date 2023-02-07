39f7a62fafa46404d4ba08f4a2a981f4f2723930
Saranya Govind <saranya789@gmail.com>
2023-02-07 21:42:17 +0000

    added test cases

--- src/main/java/com/techreturners/RomanNumeralsConverter.java
+++ src/main/java/com/techreturners/RomanNumeralsConverter.java
@@ -1,7 +1,31 @@
 package com.techreturners;
 
+import java.util.HashMap;
+
 public class RomanNumeralsConverter {
-    public String convert(int number) {
-        return "I";
+    private static final int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String numerals[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+
+    public static String integerToRoman(int value) {
+        if (value > 3000 || value < 1) {
+            return "Number should be between 1 and 3000";
+        }
+
+            StringBuilder numeral = new StringBuilder();
+
+            int index = 0;
+            while (value > 0) {
+                if (value - values[index] >= 0) {
+                    numeral.append(numerals[index]);
+                    value -= values[index];
+                } else {
+                    index++;
+                }
+            }
+           // return numeral.toString();
+
+        return numeral.toString();
     }
+
 }
--- src/test/java/com/techreturners/RomanNumeralsConverterTest.java
+++ src/test/java/com/techreturners/RomanNumeralsConverterTest.java
@@ -12,10 +12,67 @@ public class RomanNumeralsConverterTest {
         RomanNumeralsConverter romanObject = new RomanNumeralsConverter();
         //arrange
         int digit =1;
-        String actual = romanObject.convert(digit);
+        String actual = romanObject.integerToRoman(digit);
         //assert
         assertEquals("I", actual);
        // assertEquals("I",romanNumeralsConverter.convert(1));
+    }
 
+    @Test
+    public void convertSeventoNumeral(){
+        //act
+        RomanNumeralsConverter romanObject = new RomanNumeralsConverter();
+        //arrange
+        int digit =7;
+        String actual = romanObject.integerToRoman(digit);
+        //assert
+        assertEquals("VII", actual);
+
+    }
+
+    @Test
+    public void convertFourtoNumeral(){
+        //act
+        RomanNumeralsConverter romanObject = new RomanNumeralsConverter();
+        //arrange
+        int digit =4;
+        String actual = romanObject.integerToRoman(digit);
+        //assert
+        assertEquals("IV", actual);
+    }
+
+    @Test
+    public void convertFourNinetyNinetoNumeral(){
+        //act
+        RomanNumeralsConverter romanObject = new RomanNumeralsConverter();
+        //arrange
+        int digit =499;
+        String actual = romanObject.integerToRoman(digit);
+        //assert
+        assertEquals("CDXCIX", actual);
+    }
+
+
+    @Test
+    public void convertThreeThousandtoNumeral(){
+        //act
+        RomanNumeralsConverter romanObject = new RomanNumeralsConverter();
+        //arrange
+        int digit =3000;
+        String actual = romanObject.integerToRoman(digit);
+        //assert
+        assertEquals("MMM", actual);
+    }
+
+    @Test
+    public void convertThreeThousandTentoNumeral(){
+        //act
+        RomanNumeralsConverter romanObject = new RomanNumeralsConverter();
+        //arrange
+        int digit =3010;
+        String actual = romanObject.integerToRoman(digit);
+        String expected = "Number should be between 1 and 3000";
+        //assert
+        assertEquals(expected, actual);
     }
 }
